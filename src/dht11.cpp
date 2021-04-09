0c6ba8dcf14b7c6079a81362db2c3e4a5357c495
li1553770945 <1553770945@qq.com>
2021-04-09 21:14:11 +0800

    first commit

---
+++ src/dht11.cpp
@@ -0,0 +1,12 @@
+#include "DHT.h"
+#define DHTPIN 16     // Digital pin connected to the DHT sensor
+
+#define DHTTYPE DHT11 // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+void dht11()
+{
+  delay(2000);                      // 等一段时间给传感器检测，一般温度和湿度各需要250毫秒
+  float h = dht.readHumidity();    //读湿度        int
+  float t = dht.readTemperature(); //读温度
+}
